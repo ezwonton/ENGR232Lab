a238666f9d1e1d02d1e3c5b4a443dec8d804e3a8
ezwonton <eric.wan.97@gmail.com>
2017-07-28 12:33:08 -0400

    week 5 edits

--- Week 5/wan_e_lab5.m
+++ Week 5/wan_e_lab5.m
@@ -8,8 +8,8 @@ y'(0) = 3
 aux eq: x^2 + 6x + 9 = 0
 solution: parabola, stable
 %}
-aux = [1 6 9];
-r = roots(aux)
+aux = [1 6 9]; % set up auxiliary coefficient
+r = roots(aux) % solving for roots
 
 %% Part b
 %{
@@ -28,18 +28,19 @@ x1' = 0
 x2' = 0
 [x1'; x2'] = [0 1; -9 -6] * [x1; x2] + [0; 0]
 %}
-A = [0 1; -9 -6];
-B = [0; 0];
-eq = A^-1 * B
+A = [0 1; -9 -6]; % setting matrix coefficients
+B = [0; 0]; % setting x' = 0
+eq = A^-1 * B % solving for equilibrium points
 
 %% Part d - Numeric Solution
-type NumericSolution.m
-tSpan = [0:0.01:4];
-xI = [-2; 3];
-[tode, yode] = ode45(@NumericSolution, tSpan, xI);
-x1 = yode(:,1);
-x2 = yode(:,2);
+type NumericSolution.m % printing function
+tSpan = 0:0.01:4; % span of t
+xI = [-2; 3]; % init conditions of x_dot (x')
+[tode, yode] = ode45(@NumericSolution, tSpan, xI); % ode45
+x1 = yode(:,1); % setting x1
+x2 = yode(:,2); % setting x2
 
+% plotting x1 vs t
 subplot(2, 2, 1)
 plot(tode, x1)
 title('x1 vs t')
@@ -48,6 +49,7 @@ ylabel('x1')
 grid on
 hold on
 
+% plotting x2 vs t
 subplot(2, 2, 3)
 plot(tode, x2)
 title('x2 vs t')
@@ -56,6 +58,7 @@ ylabel('x2')
 grid on
 hold on
 
+% plotting x2 vs x1
 subplot(2, 2, [2 4])
 plot(x1, x2)
 title('x2 vs x1')
@@ -73,6 +76,13 @@ y = dsolve('D2y + 6*Dy + 9*y = 0, y(0) == -2, Dy(0) == 3'); % solve 2nd ODE
 yprime = diff(y,1); % y'
 yFunc = matlabFunction(y); % convert y from symbolic to function
 dyFunc = matlabFunction(yprime); % convert y' from symbolic to function
+%{
+t=0:0.1:4 %new time span
+yfunc=-2*exp(-3*t) - 3*exp(-3*t).*t %the y function
+yprimefunc=3*exp(-3*t) + 9*exp(-3*t).*t; %the y prime function
+^^ for older versions of matlab! ^^
+%}
+
 t = 0:0.01:4; % t interval
 subplot(2, 2, 1)
 plot(t, yFunc(t),'o') % y vs t
