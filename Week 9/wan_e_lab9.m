68d46fe747a4b728317905d9964221212d7cc676
ezwonton <eric.wan.97@gmail.com>
2017-08-24 18:29:58 -0400

    week 9 edits

--- Week 9/wan_e_lab9.m
+++ Week 9/wan_e_lab9.m
@@ -28,51 +28,86 @@ IC = x0;
 ep = inv(A) * -B;
 x1 = matlabFunction(x1t);
 x2 = matlabFunction(x2t);
-u = matlabFunction(ut)
+u = matlabFunction(ut);
+
+% my_matlabFunction
+x1m = my_matlabFunction_v2(x1t);
+x2m = my_matlabFunction_v2(x2t);
+um = my_matlabFunction_v2(ut);
+
 t = 0:0.01:15;
 
-figure
-subplot(2,2,1);
+figure;
+subplot(3,2,1);
 plot(t,x1(t),'r', 'linewidth', 2);
 grid on;
 ylabel('x_1(t)');
-xlabel('t')
-title('x_1(t) vs t')
+xlabel('t');
+title('x_1(t) vs t');
 
-subplot(2,2,3)
-plot(t,x2(t), 'linewidth', 2)
+subplot(3,2,2); % my_matlabFunction_v2
+plot(t,double(x1m(t)),'r', 'linewidth', 2);
 grid on;
-ylabel('x_2(t)')
-xlabel('t')
-title('x_2(t) vs t')
+ylabel('x_1(t)');
+xlabel('t');
+title('x_1m(t) vs t');
 
+subplot(3,2,3);
+plot(t,x2(t),'b', 'linewidth', 2);
+grid on;
+ylabel('x_2(t)');
+xlabel('t');
+title('x_2(t) vs t');
 
-subplot(1,2,2)
-plot(t,u(t),'r', 'linewidth', 2);
+subplot(3,2,4); % my_matlabFunction_v2
+plot(t,double(x2m(t)),'b', 'linewidth', 2);
+grid on;
+ylabel('x_1(t)');
+xlabel('t');
+title('x_2m(t) vs t');
+
+subplot(3,2,5);
+plot(t,u(t),'m', 'linewidth', 2);
 grid on;
 ylabel('u(t)');
-xlabel('t')
-title('u(t) vs t')
+xlabel('t');
+title('u(t) vs t');
 
-figure
-plot(x1(t),x2(t),'g', 'linewidth', 2)
-ylabel('x_2(t)')
-xlabel('x_1(t)')
-title('x_2(t) vs x_1(t)')
-hold on
+subplot(3,2,6); % my_matlabFunction_v2
+plot(t,double(um(t)),'m', 'linewidth', 2);
+grid on;
+ylabel('u(t)');
+xlabel('t');
+title('um(t) vs t');
+
+figure;
+subplot(1,2,1)
+plot(double(x1m(t)),double(x2m(t)),'g', 'linewidth', 2);
+ylabel('x_2m(t)');
+xlabel('x_1m(t)');
+title('x_2m(t) vs x_1m(t)');
+grid on;
+
+subplot(1,2,2)
+plot(x1(t),x2(t),'g', 'linewidth', 2);
+ylabel('x_2(t)');
+xlabel('x_1(t)');
+title('x_2(t) vs x_1(t)');
+hold on;
 
 %% Part 5
-plot(IC(1),IC(2),'b*',ep(1),ep(2),'k*')
-plot(ep(1),ep(2),'ko')
-xlabel('x_1(t)'), ylabel('x_2(t)')
-title('Phase Plot')
-legend('Analytical','Init Condition','Eqil. Pt.','Location','best')
-grid on
+plot(IC(1),IC(2),'b*',ep(1),ep(2),'k*');
+plot(ep(1),ep(2),'ko');
+xlabel('x_1(t)'), ylabel('x_2(t)');
+title('Phase Plot x_2(t) vs x_1(t)');
+legend('Analytical','Init Condition','Eqil. Pt.','Location','best');
+grid on;
 
 %% Part 6
 %{
 a.) The phase plot approaches the equilibrium point which is where the
-pulse turns off and then the plot returns to the initial condition
+pulse turns off and then the plot returns to the initial condition. Goes up
+and right first, then down and left when pulse is turned off to return to IC.
 b.) At around 12 seconds, x1(t) goes to 0
 c.) At around 12 seconds, x2(t) goes to 0
 %}
